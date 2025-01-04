be26f7f981ae732c61f505503391bc0a3ea19c4e
kannna5296 <kannnahacks5296@gmail.com>
2025-01-05 00:50:07 +0900

    相対パス検証2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/kyotsu/',
+  base: '', // 空文字列を指定して相対パスに設定
   build: {
     outDir: 'dist',  // ビルド成果物をdocsフォルダに出力
   },
