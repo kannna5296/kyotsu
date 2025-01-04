056635513d76effdfd9127402adea89cc4ef1e24
kannna5296 <kannnahacks5296@gmail.com>
2025-01-04 23:45:48 +0900

    githubpagesで公開したいのでdocsで成果物を出すように修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'docs',  // ビルド成果物をdocsフォルダに出力
+  },
 })
